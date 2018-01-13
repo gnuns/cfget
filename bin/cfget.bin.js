4dfff0f7a97598a60cbd924f7b8fc11a9ca537b5
Gabriel <gabriel@multiverso.me>
2018-01-13 15:19:23 -0200

    Rewrite to use got and be a lib

--- bin/cfget.bin.js
+++ bin/cfget.bin.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
+
+const got = require('got')
+
 try {
-  require('../cfget')();
+  const urlToGet = process.argv[2] || process.argv[1]
+  const page = require('../cfget')(urlToGet)
+  page
+  .then(async (request) => {
+    const response = await got(request.url, request)
+    console.log(response.body)
+  })
 } catch (e) {
-  process.exit(1);
+  console.log(e)
+  process.exit(1)
 }
--- cfget.js
+++ cfget.js
@@ -1,44 +1,53 @@
-const Zombie = require('zombie');
-const request = require('request');
+const {URL} = require('url')
+const Zombie = require('zombie')
 
-function cfget() {
-  let browser = new Zombie({
-    userAgent: 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0'
-  });
-  let urlToGet = process.argv[2] ? process.argv[2] : process.argv[1];
+function cfget(urlToGet, userAgent = false) {
+  userAgent = userAgent || 'Mozilla/5.0 (X11 Ubuntu Linux x86_64 rv:51.0) Gecko/20100101 Firefox/51.0'
+  urlToGet = new URL(urlToGet)
 
-  browser.visit(urlToGet)
-  .then(function() {})
-  .catch(function(error) {
-    // code 502 is expected
-    browser.wait({ duration: 6000 });
-  });
+  if (!urlToGet) return false
 
-  browser.on('redirect', function(req, res) {
-    res.text().then(function(content){
-      let additionalCookie = null;
-      for (let header of res.headers['_headers']) {
-        if (header[0] === 'set-cookie') {
-          additionalCookie = header[0].split(';')[0];
+  const browser = new Zombie({userAgent})
+
+  return new Promise (async (resolve, reject) => {
+    let solved = false
+    try {
+      await browser.visit(urlToGet.toString())
+    } catch (e) {
+      // code 502 is expected
+      browser
+      .wait({ duration: 6000 })
+      .catch(() => !solved ? resolve(false) : undefined)
+    }
+
+    browser.on('redirect', async (req, res) => {
+      try {
+        const content = await res.text()
+        let additionalCookie = null
+        for (let header of res.headers['_headers']) {
+          if (header[0] === 'set-cookie') {
+            additionalCookie = header[0].split('')[0]
+          }
         }
-      }
-      let requesOptions = {
-        'url': req.url,
-        'headers': {}
-      };
+        const request = {
+          url: req.url,
+          headers: {}
+        }
 
-      for (let header of req.headers['_headers']) {
-        requesOptions.headers[header[0]] = header[1];
+        for (let header of req.headers['_headers']) {
+          request.headers[header[0]] = header[1]
+        }
+        if (req.url.includes(urlToGet) && !solved) {
+          solved = true
+          return resolve(request)
+        }
+      } catch (e) {
+        if (!solved) resolve(false)
+        // console.log(e)
+        // reject(e)
       }
-      requesOptions.cookie += `;${additionalCookie}`;
-      request(requesOptions, function(error, response) {
-        if (error) return;
-        console.log(response.body);
-        process.exit(1);
-      });
     })
-    .catch(function(){});
-  });
+  })
 }
 
-module.exports = cfget;
+module.exports = cfget
